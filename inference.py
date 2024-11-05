87a190ca85fa27b69360d6d1d23209cf722e3f41
yookyung <susan6439@naver.com>
2024-11-05 15:55:27 +0500

    Feat: CORD dataset 다운로드 정리

--- inference.py
+++ inference.py
@@ -18,13 +18,22 @@ from utils.argeParser import parse_args
 CHECKPOINT_EXTENSIONS = ['.pth', '.ckpt']
 LANGUAGE_LIST = ['chinese', 'japanese', 'thai', 'vietnamese']
 
-def do_inference(model, ckpt_fpath, data_dir, input_size, batch_size, split='test'):
+def do_inference(input_size, batch_size, data_dir, model_dir, pth_path, device, output_dir, output_fname, split='test'):
+    model = EAST(pretrained=False).to(device)
+
+    # Get paths to checkpoint files
+    ckpt_fpath = osp.join(model_dir, pth_path+'.pth')
+
+    if not osp.exists(output_dir):
+        os.makedirs(output_dir)
+
     model.load_state_dict(torch.load(ckpt_fpath, map_location='cpu'))
     model.eval()
 
     image_fnames, by_sample_bboxes = [], []
-
     images = []
+
+    print('Inference in progress')
     
     for image_fpath in tqdm(sum([glob(osp.join(data_dir, f'{lang}_receipt/img/{split}/*')) for lang in LANGUAGE_LIST], [])):
         image_fnames.append(osp.basename(image_fpath))
@@ -42,30 +51,12 @@ def do_inference(model, ckpt_fpath, data_dir, input_size, batch_size, split='tes
         words_info = {idx: dict(points=bbox.tolist()) for idx, bbox in enumerate(bboxes)}
         ufo_result['images'][image_fname] = dict(words=words_info)
 
-    return ufo_result
+    with open(osp.join(output_dir, output_fname+'.csv'), 'w') as f:
+        json.dump(ufo_result, f, indent=4)
 
 
 def main(args):
-    # Initialize model
-    model = EAST(pretrained=False).to(args['device'])
-
-    # Get paths to checkpoint files
-    ckpt_fpath = osp.join(args['model_dir'], 'latest.pth')
-
-    if not osp.exists(args['output_dir']):
-        os.makedirs(args['output_dir'])
-
-    print('Inference in progress')
-
-    ufo_result = dict(images=dict())
-    split_result = do_inference(model, ckpt_fpath, args['data_dir'], args['input_size'],
-                                args['batch_size'], split='test')
-    ufo_result['images'].update(split_result['images'])
-
-    output_fname = 'output.csv'
-    with open(osp.join(args['output_dir'], output_fname), 'w') as f:
-        json.dump(ufo_result, f, indent=4)
-
+    do_inference(**args.__dict__)
 
 if __name__ == '__main__':
     args = parse_args('inference')
