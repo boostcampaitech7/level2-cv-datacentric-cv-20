6e2921a1d979a7fdd470f30720403f0199eb04e1
yookyung <susan6439@naver.com>
2024-11-05 15:55:13 +0500

    Feat: sharpening 구현

--- data_loader/transform.py
+++ data_loader/transform.py
@@ -1,7 +1,11 @@
 import albumentations as A
+import cv2
+import numpy as np
+from albumentations.core.transforms_interface import ImageOnlyTransform
 
 def get_train_transform():
     return A.Compose([
+        # Sharpen(),
         A.ColorJitter(),
         A.Normalize(mean=(0.5, 0.5, 0.5), std=(0.5, 0.5, 0.5)) 
     ])
@@ -11,3 +15,12 @@ def get_val_transform():
         A.ColorJitter(),
         A.Normalize(mean=(0.5, 0.5, 0.5), std=(0.5, 0.5, 0.5)) 
     ])
+
+class Sharpen(ImageOnlyTransform):
+    def __init__(self, always_apply=False, p=0.5):
+        super(Sharpen, self).__init__(always_apply=always_apply, p=p)  # 인자 이름을 명시적으로 지정
+        self.kernel = np.array([[-1, -1, -1], [-1, 9, -1], [-1, -1, -1]])
+    
+    def apply(self, image, **params):
+        sharpened = cv2.filter2D(image, -1, self.kernel)
+        return sharpened
